178c5f353b79b8667f5b52b93a5e53e82bd5bbc7
gayeongX0916 <gayeongx0916@gmail.com>
2025-05-16 13:51:17 +0900

    [feature] localStoarge 추가

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const addBtn = document.querySelector(".todo-add-btn");
 const todoList = document.querySelector(".todo-list");
 const todoInput = document.querySelector(".todo-input");
 
-addBtn.addEventListener("click", () => {
-  const text = todoInput.value.trim();
-  if (text === "") return;
-
+function createTodo(text, isDone = false) {
   // todo 글자
   const todoText = document.createElement("span");
   todoText.textContent = text;
   todoText.className = "todo-text";
+  if (isDone) todoText.classList.add("done");
 
   // 완료 버튼
   const doneBtn = document.createElement("input");
   doneBtn.type = "checkbox";
+  doneBtn.checked = isDone;
 
   // 글자 + 버튼 묶은 거
   const leftWrapper = document.createElement("div");
@@ -35,20 +34,52 @@ addBtn.addEventListener("click", () => {
   // 완료 버튼 기능
   doneBtn.addEventListener("click", () => {
     todoText.classList.toggle("done");
+
+    saveItems();
   });
 
   // 삭제 버튼 기능
   deleteBtn.addEventListener("click", () => {
     todoItem.remove();
+
+    saveItems();
   });
 
   todoList.appendChild(todoItem);
 
   todoInput.value = "";
-});
+
+  saveItems();
+}
+
+function handleAddTodo() {
+  const text = todoInput.value.trim();
+  if (text === "") return;
+
+  createTodo(text);
+}
+
+addBtn.addEventListener("click", handleAddTodo);
 
 todoInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
-    addBtn.click();
+    handleAddTodo();
   }
 });
+
+function saveItems() {
+  const todos = Array.from(todoList.children).map((li) => ({
+    contents: li.querySelector("span").textContent,
+    done: li.querySelector("span").classList.contains("done"),
+  }));
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function loadItems() {
+  const savedTodoItems = JSON.parse(localStorage.getItem("todos")) || [];
+  savedTodoItems.forEach(({ contents, done }) => {
+    createTodo(contents, done);
+  });
+}
+
+loadItems();
